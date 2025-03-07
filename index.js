8a57e68db9969081a9c9cfc2d241a7208200b688
irfandhamhudi <irfandhamhudi712@gmail.com>
2025-03-07 13:52:14 +0700

    index js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true, // Membolehkan cookies dikirimkan
   })
 );
